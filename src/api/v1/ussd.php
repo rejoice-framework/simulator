e17ceed22a6b2813dff5fddb39f9a8363a2657f8
CinPer <damien.dorcis@yahoo.com>
2020-08-17 13:50:42 +0000

    Update ussd.php

--- src/api/v1/ussd.php
+++ src/api/v1/ussd.php
@@ -1,7 +1,7 @@
 <?php
-require_once __DIR__ . '/../../../../../autoload.php';
+require_once __DIR__.'/../../../../../autoload.php';
 
-use Prinx\Simulator\Libs\Simulator;
+use Rejoice\Simulator\Libs\Simulator;
 
 $simulator = new Simulator;
 $response = $simulator->simulate();
