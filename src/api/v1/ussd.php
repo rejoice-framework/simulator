6b21f62ac054f8e8fbfb5282737dbfe19bf1c725
CinPer <damien.dorcis@yahoo.com>
2020-11-05 11:11:08 +0000

    Fix API when using in local

--- src/api/v1/ussd.php
+++ src/api/v1/ussd.php
@@ -1,7 +1,9 @@
 <?php
 
-require_once __DIR__.'/../../../../../autoload.php';
+$hostAutoload = __DIR__.'/../../../../../autoload.php';
+$localAutolaod = __DIR__.'/../../../vendor/autoload.php';
 
+require_once file_exists($hostAutoload) ? $hostAutoload : $localAutolaod;
 use Rejoice\Simulator\Libs\Simulator;
 
 $simulator = new Simulator;
